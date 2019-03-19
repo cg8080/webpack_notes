fb414921793fbc15ba4f9c8d87e058750e9e9449
finechen <finechen@jidouauto.com>
2019-03-20 00:07:12 +0800

    manage output

---
+++ 03manageOutput/src/index.js
@@ -0,0 +1,12 @@
+import printMe from './print.js'
+
+function component(){
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+  element.innerHTML = 'Hello Webpack';
+  btn.innerHTML = 'Click me and check console!';
+  btn.onclick = printMe;
+  element.append(btn);
+  return element;
+}
+document.body.appendChild(component());
